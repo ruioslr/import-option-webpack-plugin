2426a601659789ac91bae6cf07e664f1cf139294
ruios <ruiosli@kreditme.id>
2019-11-12 17:37:46 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const get = require('lodash.get');
+
+const PLUGIN_NAME = 'import-option-webpack-plugin'
+
+const mtpl = function(tpl, data) {
+    var re = /{{(.+?)}}/g;
+    while((match = re.exec(tpl))!==null) {
+        if(match[1]){
+            tpl = tpl.replace(match[0], get(data,match[1]));
+        }else{
+            tpl = tpl.replace(match[0], '')
+        }    
+    }
+    return tpl
+}
+
+
+
+module.exports = class ImportOptionWebpackPlugin {
+  
+    constructor(options) {
+      this.options = options;
+    }
+  
+    apply(resolver) {
+  
+      resolver.getHook('described-resolve').tapAsync(PLUGIN_NAME, (request, resolveContext, callback) => {
+        const innerRequest = request.request || request.path;
+        const reg = /{{(.+?)}}/;
+
+        if(reg.test(innerRequest)){
+            request = {
+                ...request,
+                request: mtpl(innerRequest, this.options),
+            }
+            console.log(request)
+            return resolver.doResolve('resolve', request, `${PLUGIN_NAME} done`, resolveContext, callback);
+        }
+        return callback();
+      });
+    }
+    
+  }
